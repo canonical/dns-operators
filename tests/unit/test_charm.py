899676878d3937c9501bc5d7327571aada2dab49
Arturo Seijas <arturo.seijas@canonical.com>
2024-04-03 10:07:18 +0200

    Add minimal docs and descriptors

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python3
-
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Integration tests."""
-
-import pytest
+"""Unit tests for the charm."""
 
 
-@pytest.mark.abort_on_fail
-async def test_nothing():
+def test_nothing():
     """
     arrange: nothing.
     act: nothing.

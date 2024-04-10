15ff890a2d77c6f94b96648ef2a77768d8954cf3
Arturo Seijas <arturo.seijas@canonical.com>
2024-04-10 10:07:30 +0200

    fix libpatch

--- lib/charms/bind/v0/dns_record.py
+++ lib/charms/bind/v0/dns_record.py
@@ -64,7 +64,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 8
+LIBPATCH = 1
 
 PYDEPS = ["pydantic>=2"]
 

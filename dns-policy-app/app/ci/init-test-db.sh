4b660deca098e81b7041cd9fa264387a2dfd9c31
Niels Robin-Aubertin <git@niels.fr>
2025-01-30 18:05:11 -0500

    Add workflow to test the app

---
+++ dns-policy-app/app/ci/init-test-db.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Meant to be executed after installing postgres on ubuntu
+# (for the CI)
+
+DB_NAME="testname"
+DB_USER="testuser"
+DB_PASSWORD="testpassword"
+
+sudo -u postgres psql <<EOF
+CREATE DATABASE $DB_NAME;
+CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';
+ALTER ROLE $DB_USER SET client_encoding TO 'utf8';
+ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';
+ALTER ROLE $DB_USER SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
+\c $DB_NAME;
+GRANT ALL ON SCHEMA public TO $DB_USER;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_USER;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $DB_USER;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO $DB_USER;
+EOF

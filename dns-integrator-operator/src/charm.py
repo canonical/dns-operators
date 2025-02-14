c18a6baf14015694c56065d6397b0081b62b0a0e
Niels Robin-Aubertin <git@niels.fr>
2025-02-14 09:04:49 -0500

    Add blockedStatus if updating relation data didn't work

--- dns-integrator-operator/src/charm.py
+++ dns-integrator-operator/src/charm.py
@@ -8,7 +8,6 @@
 import logging
 import typing
 import hashlib
-import uuid
 
 import ops
 from charms.bind.v0 import dns_record
@@ -46,6 +45,7 @@ class DnsIntegratorOperatorCharm(ops.CharmBase):
                 )
         except ops.model.ModelError as e:
             logger.error("ERROR while updating relation data: %s", e)
+            self.unit.status = ops.BlockedStatus("Error while updating relation data.")
 
     def _get_dns_record_requirer_data(self) -> dns_record.DNSRecordRequirerData:
         """Get DNS record requirer data."""

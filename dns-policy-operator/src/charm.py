40ea35c3b5a55367a8a63835073ffa8f7297510f
Niels Robin-Aubertin <git@niels.fr>
2025-02-26 14:44:04 -0500

    Address comments

--- dns-policy-operator/src/charm.py
+++ dns-policy-operator/src/charm.py
@@ -28,7 +28,7 @@ logger = logging.getLogger(__name__)
 VALID_LOG_LEVELS = ["info", "debug", "warning", "error", "critical"]
 
 
-class ReconciliateEvent(ops.charm.EventBase):
+class ReconcileEvent(ops.charm.EventBase):
     """Event representing a periodic reload of the charmed-bind service."""
 
 
@@ -43,9 +43,7 @@ class DnsPolicyCharm(ops.CharmBase):
         """
         super().__init__(*args)
 
-        self.on.define_event("reconciliate", ReconciliateEvent)
-        logger.debug("unit name: %s", self.unit.name)
-
+        self.on.define_event("reconcile", ReconcileEvent)
         self.dns_policy = DnsPolicyService()
         self._database = DatabaseHandler(self, constants.DATABASE_RELATION_NAME)
         self.dns_record_provider = dns_record.DNSRecordProvides(self, "dns-record-provider")
@@ -68,23 +66,22 @@ class DnsPolicyCharm(ops.CharmBase):
             self.dns_record_provider.on.dns_record_request_received,
             self._on_dns_record_request_received,
         )
-        self.framework.observe(self.on.reconciliate, self._on_reconciliate)
+        self.framework.observe(self.on.reconcile, self._on_reconcile)
         self.unit.open_port("tcp", 8080)  # dns-policy-app
 
-    def _on_reconciliate(self, _: ReconciliateEvent) -> None:
-        relation_data = self._get_remote_relation_data()
-        if relation_data is None:
+    def _on_reconcile(self, _: ReconcileEvent) -> None:
+        if not self.model.unit.is_leader():
             return
-        dns_policy_config = self.dns_policy.get_config()
+        relation_data = self.dns_record_provider.get_remote_relation_data()
+        if relation_data is None:
+            logger.debug("Reconciliation: no provider relation data found")
+            return
         entries: list[models.DnsEntry] = []
         for record_requirer_data, _ in relation_data:
             for entry in record_requirer_data.dns_entries:
                 entries.append(entry)
-        # logger.debug("Entries: %s", entries)
         if not entries:
-            return
-        logger.debug("Config: %s", dns_policy_config)
-        if not self.model.unit.is_leader():
+            logger.debug("Reconciliation: no entry found in provider data")
             return
 
         token = self.dns_policy.get_api_root_token()
@@ -93,23 +90,7 @@ class DnsPolicyCharm(ops.CharmBase):
         approved_requests = self.dns_policy.get_approved_requests(token)
         dns_record_requirer_data = dns_record.DNSRecordRequirerData(dns_entries=approved_requests)
         for relation in self.model.relations[self.dns_record_requirer.relation_name]:
-            logger.debug("dnsrecordrequirerdata: %s", dns_record_requirer_data)
             self.dns_record_requirer.update_relation_data(relation, dns_record_requirer_data)
-
-    def _on_dns_record_request_received(self, event: dns_record.DNSRecordRequestReceived) -> None:
-        """Handle dns record request received."""
-        # relation_data = self._get_remote_relation_data()
-        # if relation_data is None:
-        #     return
-        # dns_policy_config = self.dns_policy.get_config()
-        # entries = self.dns_record_relations_data_to_entries(relation_data)
-        # logger.debug("Entries: %s", entries)
-        # if not entries:
-        #     return
-        # logger.debug("Config: %s", dns_policy_config)
-        # token = self.dns_policy.get_api_root_token()
-        # for entry in entries:
-        #     self.dns_policy.create_record_request(token, entry)
 
     def _start_timer(self, event_name: str, timeout: str, interval: str) -> None:
         """Install a timer.
@@ -151,29 +132,6 @@ class DnsPolicyCharm(ops.CharmBase):
         systemd.service_disable(f"dispatch-{event_name}.timer")
         systemd.service_stop(f"dispatch-{event_name}.timer")
 
-    def dns_record_relations_data_to_zones(
-        self,
-        relation_data: list[
-            tuple[dns_record.DNSRecordRequirerData, dns_record.DNSRecordProviderData]
-        ],
-    ) -> list[models.Zone]:
-        """Return zones from all the dns_record relations data.
-
-        Args:
-            relation_data: input relation data
-
-        Returns:
-            The zones from the record_requirer_data
-        """
-        zones: dict[str, models.Zone] = {}
-        for record_requirer_data, _ in relation_data:
-            for new_zone in self.record_requirer_data_to_zones(record_requirer_data):
-                if new_zone.domain in zones:
-                    zones[new_zone.domain].entries.update(new_zone.entries)
-                else:
-                    zones[new_zone.domain] = new_zone
-        return list(zones.values())
-
     def dns_record_relations_data_to_entries(
         self,
         relation_data: list[
@@ -192,47 +150,6 @@ class DnsPolicyCharm(ops.CharmBase):
             for entry in record_requirer_data.dns_entries:
                 entries.append(models.create_dns_entry_from_requirer_entry(entry))
         return entries
-
-    def record_requirer_data_to_zones(
-        self,
-        record_requirer_data: dns_record.DNSRecordRequirerData,
-    ) -> list[models.Zone]:
-        """Convert DNSRecordRequirerData to zone files.
-
-        Args:
-            record_requirer_data: The input DNSRecordRequirerData
-
-        Returns:
-            A list of zones
-        """
-        zones_entries: dict[str, list[models.RequirerEntry]] = {}
-        for entry in record_requirer_data.dns_entries:
-            if entry.domain not in zones_entries:
-                zones_entries[entry.domain] = []
-            zones_entries[entry.domain].append(entry)
-
-        zones: list[models.Zone] = []
-        for domain, entries in zones_entries.items():
-            zone = models.Zone(domain=domain, entries=set())
-            for entry in entries:
-                zone.entries.add(models.create_dns_entry_from_requirer_entry(entry))
-            zones.append(zone)
-        return zones
-
-    def _get_remote_relation_data(self) -> dns_record.DNSRecordProviderData | None:
-        """Get the dns_record remote relation data.
-
-        This function is used to get performance statistics on the remote relation data retrieval.
-
-        Returns:
-            the dns_record remote relation data
-        """
-        start_time = time.time_ns()
-        relation_data = self.dns_record_provider.get_remote_relation_data()
-        logger.debug(
-            "Relation data retrieval duration (ms): %s", (time.time_ns() - start_time) / 1e6
-        )
-        return relation_data
 
     def _on_config_changed(self, event: ops.ConfigChangedEvent) -> None:
         """Handle changed configuration."""
@@ -264,7 +181,7 @@ class DnsPolicyCharm(ops.CharmBase):
         self.unit.status = ops.MaintenanceStatus("Preparing dns-policy-app")
         self.dns_policy.setup(self.unit.name)
         self.unit.status = ops.ActiveStatus("")
-        self._start_timer("reconciliate", "60s", "1m")
+        self._start_timer("reconcile", "60s", "1m")
 
     def _on_database_created(self, _: DatabaseCreatedEvent) -> None:
         """Handle database created.
@@ -273,20 +190,6 @@ class DnsPolicyCharm(ops.CharmBase):
             event: Event triggering the database created handler.
         """
         database_relation_data = self._database.get_relation_data()
-        logger.info(
-            "%s",
-            {
-                "debug": "true" if self.config["debug"] else "false",
-                "allowed-hosts": json.dumps(
-                    [e.strip() for e in str(self.config["allowed-hosts"]).split(",")]
-                ),
-                "database-port": database_relation_data["POSTGRES_PORT"],
-                "database-host": database_relation_data["POSTGRES_HOST"],
-                "database-name": database_relation_data["POSTGRES_DB"],
-                "database-password": database_relation_data["POSTGRES_PASSWORD"],
-                "database-user": database_relation_data["POSTGRES_USER"],
-            },
-        )
         self.dns_policy.configure(
             {
                 "debug": "true" if self.config["debug"] else "false",

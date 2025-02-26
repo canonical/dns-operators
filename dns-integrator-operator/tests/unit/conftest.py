2313500552153e6918c037cfe55e64db18727cbf
Niels Robin-Aubertin <nrobinaubertin@users.noreply.github.com>
2025-02-26 09:08:51 -0500

    Add dns-integrator-operator (#79)
    * Add dns-integrator-operator
    
    * Fix copyright year
    
    * Fixes
    
    * Remove global repo files
    
    * Address comments
    
    * Fix
    
    * Improve in-house uuid function
    
    * Add blockedStatus if updating relation data didn't work
    
    * Go into error state if the rel data couldn't be updated
    
    * Add tests and improve status of the charm
    
    * Use uuid5 instead of in-house function
    
    * Address comments
    
    * Improve comment
    
    * Fix licenses

---
+++ dns-integrator-operator/tests/unit/conftest.py
@@ -0,0 +1,26 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Fixtures for charm tests."""
+
+import logging
+from unittest.mock import Mock
+
+import pytest
+
+from src.charm import DnsIntegratorCharm
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def mock_self():
+    """Create a mock self object since we don't need real instance."""
+    return Mock(spec=DnsIntegratorCharm)
+
+
+@pytest.fixture(name="base_state")
+def base_state_fixture():
+    """Base state fixture."""
+    input_state: dict = {}
+    yield input_state

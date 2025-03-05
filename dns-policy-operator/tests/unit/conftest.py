de65e55e7dcab808df373b2114ef030f8854b8a4
Niels Robin-Aubertin <git@niels.fr>
2025-03-05 14:58:11 -0500

    Improve unit tests

--- dns-policy-operator/tests/unit/conftest.py
+++ dns-policy-operator/tests/unit/conftest.py
@@ -3,8 +3,9 @@
 
 """Fixtures for charm tests."""
 
+import json
 import logging
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 import ops.testing
 import pytest
@@ -15,31 +16,43 @@ from src.charm import DnsPolicyCharm
 logger = logging.getLogger(__name__)
 
 
+@pytest.fixture(name="api_root_token")
+def api_root_token_fixture():
+    """API root token fixture."""
+    yield "SomeTestApiRootToken"
+
+
 @pytest.fixture(name="context")
-def context_fixture():
+def context_fixture(api_root_token):
     """Context fixture."""
     with (
         patch("timer.TimerService.start"),
         patch("dns_policy.DnsPolicyService.setup"),
         patch("dns_policy.DnsPolicyService.status") as dns_policy_status,
         patch("dns_policy.DnsPolicyService.configure"),
+        patch("dns_policy.DnsPolicyService.get_approved_requests"),
+        patch("dns_policy.DnsPolicyService.get_api_root_token") as dns_policy_get_api_root_token,
     ):
         dns_policy_status.return_value = True
+        dns_policy_get_api_root_token.return_value = api_root_token
         yield ops.testing.Context(
             charm_type=DnsPolicyCharm,
         )
-
-
-@pytest.fixture
-def mock_self():
-    """Create a mock self object since we don't need real instance."""
-    return Mock(spec=DnsPolicyCharm)
 
 
 @pytest.fixture(name="base_state")
 def base_state_fixture():
     """Base state fixture."""
     input_state: dict = {"leader": True}
+    input_state["relations"] = [
+        scenario.SubordinateRelation(
+            endpoint="dns-record-requirer",
+            interface="dns_record",
+            remote_app_name="bind",
+            remote_app_data={},
+            local_unit_data={},
+        )
+    ]
     yield input_state
 
 
@@ -56,6 +69,33 @@ def database_relation_fixture():
         endpoint="database",
         interface="database",
         remote_app_name="postgresql",
+        remote_app_data=data,
+        local_unit_data=data,
+    )
+
+
+@pytest.fixture(name="record_request")
+def record_request_fixture():
+    """Record request fixture."""
+    yield {
+        "domain": "canonical.com",
+        "host_label": "admin",
+        "ttl": 3600,
+        "record_class": "IN",
+        "record_type": "A",
+        "record_data": "204.45.64.14",
+        "uuid": "2c210a7c-55fe-52e1-a14b-2268bd8f4669",
+    }
+
+
+@pytest.fixture(name="requirer_relation")
+def requirer_relation_fixture(record_request):
+    """Requirer relation data fixture."""
+    data = {"dns_entries": json.dumps([record_request])}
+    return scenario.Relation(
+        endpoint="dns-record-provider",
+        interface="dns_record",
+        remote_app_name="dns-integrator",
         remote_app_data=data,
         local_unit_data=data,
     )

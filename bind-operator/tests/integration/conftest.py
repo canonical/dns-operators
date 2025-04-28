1ef1f71fe89ef809f96e1ec0d09babdbcca21f56
Niels Robin-Aubertin <git@niels.fr>
2025-04-28 16:55:28 -0400

    Try to pack charm differently in bind conftest

--- bind-operator/tests/integration/conftest.py
+++ bind-operator/tests/integration/conftest.py
@@ -3,39 +3,63 @@
 
 """Integration tests fixtures."""
 
-from pathlib import Path
-
+import typing
 import pytest_asyncio
 import yaml
-from pytest import Config, fixture
+import pytest
+import pathlib
+import subprocess
 from pytest_operator.plugin import Model, OpsTest
 
 
-@fixture(scope="module", name="metadata")
+@pytest.fixture(scope="module", name="metadata")
 def fixture_metadata():
     """Provide charm metadata."""
-    yield yaml.safe_load(Path("./metadata.yaml").read_text(encoding="UTF-8"))
+    yield yaml.safe_load(pathlib.Path("./metadata.yaml").read_text(encoding="UTF-8"))
 
 
-@fixture(scope="module", name="app_name")
+@pytest.fixture(scope="module", name="app_name")
 def fixture_app_name(metadata):
     """Provide app name from the metadata."""
     yield metadata["name"]
 
 
-@fixture(scope="module", name="model")
+@pytest.fixture(scope="module", name="model")
 def model_fixture(ops_test: OpsTest) -> Model:
     """Juju model API client."""
     assert ops_test.model
     return ops_test.model
 
 
+@pytest.fixture(scope="session")
+def charm_file(metadata: dict[str, typing.Any], pytestconfig: pytest.Config):
+    """Pytest fixture that packs the charm and returns the filename, or --charm-file if set."""
+    charm_file = pytestconfig.getoption("--charm-file")
+    if charm_file:
+        yield charm_file
+        return
+    try:
+        subprocess.run(
+            ["charmcraft", "pack"], check=True, capture_output=True, text=True
+        )  # nosec B603, B607
+    except subprocess.CalledProcessError as exc:
+        raise OSError(f"Error packing charm: {exc}; Stderr:\n{exc.stderr}") from None
+
+    app_name = metadata["name"]
+    charm_path = pathlib.Path(__file__).parent.parent.parent
+    charms = [p.absolute() for p in charm_path.glob(f"{app_name}_*.charm")]
+    assert charms, f"{app_name}.charm file not found"
+    assert len(charms) == 1, f"{app_name} has more than one .charm file, unsure which to use"
+    yield str(charms[0])
+
+
 @pytest_asyncio.fixture(scope="module", name="app")
 async def app_fixture(
     ops_test: OpsTest,
     app_name: str,
-    pytestconfig: Config,
+    pytestconfig: pytest.Config,
     model: Model,
+    charm_file: str,
 ):
     """Build the charm and deploys it."""
     use_existing = pytestconfig.getoption("--use-existing", default=False)
@@ -48,13 +72,9 @@ async def app_fixture(
     if pytestconfig.getoption("--charmed-bind-snap-file"):
         resources.update({"charmed-bind-snap": pytestconfig.getoption("--charmed-bind-snap-file")})
 
-    if charm := pytestconfig.getoption("--charm-file"):
-        application = await model.deploy(
-            f"./{charm}", application_name=app_name, resources=resources
-        )
-    else:
-        charm = await ops_test.build_charm(".")
-        application = await model.deploy(charm, application_name=app_name, resources=resources)
+    application = await model.deploy(
+        f"./{charm_file}", application_name=app_name, resources=resources
+    )
 
     await model.wait_for_idle(apps=[application.name], status="active")
 

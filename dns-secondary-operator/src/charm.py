e0ebeb8a04a70a0462f0fc88d34d2ccb62bb97f4
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2025-07-30 17:05:41 -0300

    Remove _check_and_may_become_active

--- dns-secondary-operator/src/charm.py
+++ dns-secondary-operator/src/charm.py
@@ -5,8 +5,6 @@
 """Charm for dns-secondary."""
 
 import logging
-import subprocess  # nosec
-import time
 import typing
 
 import ops
@@ -95,14 +93,6 @@ class DnsSecondaryCharm(ops.CharmBase):
         if not self._has_required_integration():
             return
 
-        try:
-            t = self.topology.current()
-        except topology.TopologyUnavailableError as err:
-            logger.info("Could not retrieve network topology: %s", err)
-            return
-        if self.unit.is_leader() and not t.is_current_unit_active:
-            self._check_and_may_become_active(t)
-
         self.unit.status = ops.MaintenanceStatus("Preparing bind")
         self.bind.setup(self.unit.name)
         self.bind.start()
@@ -126,65 +116,6 @@ class DnsSecondaryCharm(ops.CharmBase):
         if data is None:
             return False
         return True
-
-    def _check_and_may_become_active(self, t: topology.Topology) -> bool:
-        """Check the active unit status and may become active if need be.
-
-        Args:
-            t: Topology of the current deployment
-
-        Returns:
-            True if the charm is effectively the new active unit.
-        """
-        relation = self.model.get_relation(constants.PEER)
-        assert relation is not None  # nosec
-        if not t.active_unit_ip:
-            relation.data[self.app].update({"active-unit": str(t.current_unit_ip)})
-            return True
-
-        status = self._dig_query(
-            f"@{t.active_unit_ip} service.{constants.ZONE_SERVICE_NAME} TXT +short",
-            retry=True,
-            wait=1,
-        )
-        if status != "ok":
-            relation.data[self.app].update({"active-unit": str(t.current_unit_ip)})
-            return True
-        return False
-
-    def _dig_query(self, cmd: str, retry: bool = False, wait: int = 5) -> str:
-        """Query a DnsEntry with dig.
-
-        This function was created for simplicity's sake. If we need to make more DNS requests
-        in the future, we should revisit it by employing a python library.
-
-        Args:
-            cmd: The dig command to perform
-            retry: If the dig request should be retried
-            wait: duration in seconds to wait between retries
-
-        Returns: the result of the DNS query
-        """
-        result: str = ""
-        retry = False
-        for _ in range(5):
-            try:
-                result = str(
-                    subprocess.run(
-                        ["dig"] + cmd.split(" "),  # nosec
-                        capture_output=True,
-                        text=True,
-                        check=True,
-                    )
-                ).strip()
-            except subprocess.CalledProcessError as exc:
-                logger.warning("%s", exc)
-                result = ""
-            if result != "" or not retry:
-                break
-            time.sleep(wait)
-
-        return result
 
 
 if __name__ == "__main__":  # pragma: nocover

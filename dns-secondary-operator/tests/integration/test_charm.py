4e86ff96570a3dc1232f9e16c13992797657a0da
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2025-07-31 14:38:42 -0300

    Run integration test with jubilant

--- dns-secondary-operator/tests/integration/test_charm.py
+++ dns-secondary-operator/tests/integration/test_charm.py
@@ -1,33 +1,30 @@
 #!/usr/bin/env python3
+
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 """Integration tests."""
 
-import logging
-
-import ops
+import jubilant
 import pytest
 
-from charm import STATUS_REQUIRED_INTEGRATION
 
-logger = logging.getLogger(__name__)
+@pytest.fixture(scope="module", name="juju")
+def juju_fixture():
+    """Juju fixture"""
+    with jubilant.temp_model() as juju:
+        yield juju
 
 
 @pytest.mark.asyncio
 @pytest.mark.abort_on_fail
 @pytest.mark.skip_if_deployed
-async def test_deploy(model, charm_file, app_name, resources):
+async def test_deploy(juju: jubilant.Juju, charm_file, app_name, resources):
     """
     arrange: build and deploy the charm.
     act: nothing.
     assert: charm is in blocked status.
     """
-    application = await model.deploy(
-        f"{charm_file}", application_name=app_name, resources=resources
-    )
-    await model.wait_for_idle(apps=[application.name], timeout=300, status="blocked")
-
-    assert application.units[0].workload_status == ops.model.BlockedStatus.name
-    status = await model.get_status()
-    assert status.applications[app_name]["status"]["info"] == STATUS_REQUIRED_INTEGRATION
+    juju.deploy(charm=charm_file, app=app_name, resources=resources)
+    juju.wait(jubilant.all_agents_idle, timeout=600)
+    juju.wait(jubilant.all_blocked)

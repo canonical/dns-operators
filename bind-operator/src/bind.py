7a3cc1e88f34caf799bc33d6bb33e00ff832e91e
Niels Robin-Aubertin <git@niels.fr>
2025-09-24 09:24:59 -0400

    Fixes

--- bind-operator/src/bind.py
+++ bind-operator/src/bind.py
@@ -307,8 +307,12 @@ class BindService:
             )
 
             # If an public ip is configured, we use it for our NS records
-            if topology.public_ips:
-                ns_ip_list: list[pydantic.IPvAnyAddress] = topology.public_ips
+            if config.get("public_ips"):
+                ns_ip_list: list[pydantic.IPvAnyAddress] = [
+                    ip.strip()
+                    for ip in config.get("public-ips", "").split(",")
+                    if ip.strip() != ""
+                ]
             else:
                 # By default, we hide the active unit.
                 # So only the standbies are used to respond to queries and receive NOTIFY events
@@ -317,8 +321,12 @@ class BindService:
                 ns_ip_list = topology.standby_units_ip or [topology.current_unit_ip]
 
             # If an name list is configured, we use it for our NS records
-            if topology.names:
-                ns_name_list: list[str] = topology.names
+            if config.get("names"):
+                ns_name_list: list[str] = [
+                    name.strip()
+                    for name in config.get("names", "").split(",")
+                    if name.strip() != ""
+                ]
             else:
                 # By default we just use "ns" as host_label
                 # from the served domain for the nameserver

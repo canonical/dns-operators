7b1509bd7c9fbd20f09685550c30293eeebb1ffb
Niels Robin-Aubertin <git@niels.fr>
2025-10-01 17:40:24 -0400

    Remove some scenario imports

--- bind-operator/tests/unit/test_charm.py
+++ bind-operator/tests/unit/test_charm.py
@@ -10,8 +10,6 @@ from unittest.mock import ANY, patch
 
 import ops
 import pytest
-import scenario
-from ops import testing
 from scenario.context import _Event  # needed for custom events for now
 
 import tests.unit.helpers
@@ -28,9 +26,9 @@ def test_start_without_peer_relation(context, base_state):
     assert: status is peer not available
     """
     base_state["relations"] = []
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     out = context.run(context.on.start(), state)
-    assert out.unit_status == testing.WaitingStatus("Topology is not available")
+    assert out.unit_status == ops.testing.WaitingStatus("Topology is not available")
 
 
 @pytest.mark.usefixtures("context")
@@ -41,9 +39,9 @@ def test_start(context, base_state):
     act: run start
     assert: status is active
     """
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     out = context.run(context.on.start(), state)
-    assert out.unit_status == testing.ActiveStatus()
+    assert out.unit_status == ops.testing.ActiveStatus()
 
 
 @pytest.mark.usefixtures("context")
@@ -54,9 +52,9 @@ def test_stop(context, base_state):
     act: run stop
     assert: status is active
     """
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     out = context.run(context.on.stop(), state)
-    assert out.unit_status == testing.ActiveStatus()
+    assert out.unit_status == ops.testing.ActiveStatus()
 
 
 @pytest.mark.usefixtures("context")
@@ -67,9 +65,9 @@ def test_install(context, base_state):
     act: run install
     assert: status is active
     """
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     out = context.run(context.on.install(), state)
-    assert out.unit_status == testing.ActiveStatus()
+    assert out.unit_status == ops.testing.ActiveStatus()
 
 
 @pytest.mark.usefixtures("context")
@@ -80,9 +78,9 @@ def test_config_changed(context, base_state):
     act: run config_changed
     assert: status is active
     """
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     out = context.run(context.on.config_changed(), state)
-    assert out.unit_status == testing.ActiveStatus()
+    assert out.unit_status == ops.testing.ActiveStatus()
 
 
 @pytest.mark.usefixtures("context")
@@ -94,9 +92,9 @@ def test_leader_elected_changed_while_leader_not_active(context, base_state):
     assert: status is active
     """
     base_state["leader"] = True
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     out = context.run(context.on.leader_elected(), state)
-    assert out.unit_status == testing.ActiveStatus("active")
+    assert out.unit_status == ops.testing.ActiveStatus("active")
 
 
 @pytest.mark.usefixtures("context")
@@ -109,16 +107,16 @@ def test_leader_elected_changed_while_leader_not_active_dig_timeout(context, bas
     """
     base_state["leader"] = True
     databag = {"active-unit": "1.2.99.4"}
-    peer_relation = scenario.PeerRelation(
+    peer_relation = ops.testing.PeerRelation(
         endpoint="bind-peers",
         local_app_data=databag,
     )
     base_state["relations"][0] = peer_relation
     with patch("src.charm.BindCharm._dig_query") as dig_query:
         dig_query.return_value = ""
-        state = testing.State(**base_state)
+        state = ops.testing.State(**base_state)
         out = context.run(context.on.leader_elected(), state)
-        assert out.unit_status == testing.ActiveStatus("active")
+        assert out.unit_status == ops.testing.ActiveStatus("active")
 
 
 @pytest.mark.usefixtures("context")
@@ -131,16 +129,16 @@ def test_leader_elected_changed_while_leader_not_active_dig_ok(context, base_sta
     """
     base_state["leader"] = True
     databag = {"active-unit": "1.2.99.4"}
-    peer_relation = scenario.PeerRelation(
+    peer_relation = ops.testing.PeerRelation(
         endpoint="bind-peers",
         local_app_data=databag,
     )
     base_state["relations"][0] = peer_relation
     with patch("src.charm.BindCharm._dig_query") as dig_query:
         dig_query.return_value = "ok"
-        state = testing.State(**base_state)
+        state = ops.testing.State(**base_state)
         out = context.run(context.on.leader_elected(), state)
-        assert out.unit_status == testing.ActiveStatus()
+        assert out.unit_status == ops.testing.ActiveStatus()
 
 
 @pytest.mark.usefixtures("context")
@@ -152,9 +150,9 @@ def test_leader_elected_changed_while_not_leader(context, base_state):
     assert: status is active
     """
     base_state["leader"] = False
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     out = context.run(context.on.leader_elected(), state)
-    assert out.unit_status == testing.ActiveStatus()
+    assert out.unit_status == ops.testing.ActiveStatus()
 
 
 @pytest.mark.usefixtures("context")
@@ -167,12 +165,12 @@ def test_peer_relation_departed_while_leader(context, base_state, peer_relation)
     assert: status is active
     """
     base_state["leader"] = True
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     peer_relation_departed_event = _Event(
         f"{peer_relation.endpoint}_relation_departed", relation=peer_relation
     )
     out = context.run(peer_relation_departed_event, state)
-    assert out.unit_status == testing.ActiveStatus("active")
+    assert out.unit_status == ops.testing.ActiveStatus("active")
 
 
 @pytest.mark.usefixtures("context")
@@ -185,12 +183,12 @@ def test_peer_relation_departed_while_not_leader(context, base_state, peer_relat
     assert: status is active
     """
     base_state["leader"] = False
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     peer_relation_departed_event = _Event(
         f"{peer_relation.endpoint}_relation_departed", relation=peer_relation
     )
     out = context.run(peer_relation_departed_event, state)
-    assert out.unit_status == testing.ActiveStatus()
+    assert out.unit_status == ops.testing.ActiveStatus()
 
 
 @pytest.mark.usefixtures("context")
@@ -202,12 +200,12 @@ def test_peer_relation_joined(context, base_state, peer_relation):
     act: run peer relation joined
     assert: status is active
     """
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     peer_relation_joined_event = _Event(
         f"{peer_relation.endpoint}_relation_joined", relation=peer_relation
     )
     out = context.run(peer_relation_joined_event, state)
-    assert out.unit_status == testing.ActiveStatus()
+    assert out.unit_status == ops.testing.ActiveStatus()
 
 
 @pytest.mark.usefixtures("context")
@@ -218,10 +216,10 @@ def test_reload_bind(context, base_state):
     act: run reload bind
     assert: status is active
     """
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     reload_bind_event = _Event("reload_bind")
     out = context.run(reload_bind_event, state)
-    assert out.unit_status == testing.ActiveStatus()
+    assert out.unit_status == ops.testing.ActiveStatus()
 
 
 @pytest.mark.usefixtures("context")
@@ -245,13 +243,13 @@ def test_dns_record_relation_changed_without_conflict(context, base_state):
         "dns_entries": json.dumps(dumped_model["dns_entries"], default=str),
     }
 
-    dns_record_relation = scenario.Relation(
+    dns_record_relation = ops.testing.Relation(
         endpoint="dns-record",
         remote_app_data=dumped_data,
     )
     base_state["relations"].append(dns_record_relation)
     base_state["leader"] = True
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     dns_record_relation_changed_event = _Event(
         "dns_record_relation_changed", relation=dns_record_relation
     )
@@ -263,7 +261,7 @@ def test_dns_record_relation_changed_without_conflict(context, base_state):
             out_uuids = {x["uuid"] for x in data}
             # check that all the records from the requirer data are approved
             assert out_uuids == in_uuids
-    # Only testing if the unit has an Active status without checking the status description
+    # Only ops.testing if the unit has an Active status without checking the status description
     # This is done since we don't want to test if the unit reports as being the "active" one
     # (it's not relevant to this test).
     assert isinstance(out.unit_status, ops.ActiveStatus)
@@ -290,13 +288,13 @@ def test_dns_record_relation_changed_without_conflict_not_leader(context, base_s
         "dns_entries": json.dumps(dumped_model["dns_entries"], default=str),
     }
 
-    dns_record_relation = scenario.Relation(
+    dns_record_relation = ops.testing.Relation(
         endpoint="dns-record",
         remote_app_data=dumped_data,
     )
     base_state["relations"].append(dns_record_relation)
     base_state["leader"] = False
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     dns_record_relation_changed_event = _Event(
         "dns_record_relation_changed", relation=dns_record_relation
     )
@@ -305,7 +303,7 @@ def test_dns_record_relation_changed_without_conflict_not_leader(context, base_s
         if relation.endpoint == "dns-record":
             # As we are not the leader, we do not update the databag of the relation
             assert "dns_entries" not in relation.local_app_data
-    assert out.unit_status == testing.ActiveStatus()
+    assert out.unit_status == ops.testing.ActiveStatus()
 
 
 @pytest.mark.skip(reason="TODO: Status is not currently correctly computed.")
@@ -332,13 +330,13 @@ def test_dns_record_relation_changed_with_conflict(context, base_state):
         dumped_data = {
             "dns_entries": json.dumps(dumped_model["dns_entries"], default=str),
         }
-        dns_record_relation = scenario.Relation(
+        dns_record_relation = ops.testing.Relation(
             endpoint="dns-record",
             remote_app_data=dumped_data,
         )
         base_state["relations"].append(dns_record_relation)
     base_state["leader"] = True
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     dns_record_relation_changed_event = _Event(
         "dns_record_relation_changed", relation=dns_record_relation
     )
@@ -350,7 +348,7 @@ def test_dns_record_relation_changed_with_conflict(context, base_state):
             requests_data = json.loads(relation.local_app_data["dns_entries"])
             for request in requests_data:
                 assert request["status"] == "conflict"
-    assert out.unit_status == testing.BlockedStatus("Conflicting requests")
+    assert out.unit_status == ops.testing.BlockedStatus("Conflicting requests")
 
 
 @pytest.mark.usefixtures("context")
@@ -365,13 +363,13 @@ def test_dns_transfer_relation_changed(context, base_state):
     dumped_data = {
         "addresses": json.dumps([secondary_ip], default=str),
     }
-    dns_transfer_relation = scenario.Relation(
+    dns_transfer_relation = ops.testing.Relation(
         endpoint="dns-transfer",
         remote_app_data=dumped_data,
     )
     base_state["relations"].append(dns_transfer_relation)
     base_state["leader"] = True
-    state = testing.State(**base_state)
+    state = ops.testing.State(**base_state)
     dns_transfer_relation_changed_event = _Event(
         "dns_transfer_relation_changed", relation=dns_transfer_relation
     )

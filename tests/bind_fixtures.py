8adf157d06f71a3728f3f3f01568a6cf4df9fecc
Niels Robin-Aubertin <nrobinaubertin@users.noreply.github.com>
2025-10-07 09:53:40 -0400

    Global integ tests (#254)
    * Add fixtures and a simple test for now
    
    * Wait for idle before relating
    
    * Fix fixtures
    
    * Add triggers to bind integ tests
    
    * Add dns_policy module to integ tests
    
    * Add dns_resolver wf
    
    * Improve fixtures
    
    * Create a single global test with bind, integrator and resolver
    
    * Rewrite global integ test wf
    
    * Debug
    
    * Fix attempt
    
    * Fix
    
    * Add dns-secondry to the test
    
    * Create topology module, implement dns-authority in dns-secondary
    
    * Fix imports
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fixes
    
    * Fix
    
    * Fix
    
    * Another fix
    
    * Fix dns-secondary charm
    
    * More fixes
    
    * Add bind-charm-file option
    
    * More fixes
    
    * Lint
    
    * Change to provider for dns-authority
    
    * Fix tests
    
    * Remove some stuff from the runner
    
    * Change order
    
    * Do not wait for last snaps autorefresh
    
    * Manually install env
    
    * Bootstrap juju controller
    
    * Fix user
    
    * Pack secondary charm in advance
    
    * Fix

---
+++ tests/bind_fixtures.py
@@ -0,0 +1,72 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Integration tests fixtures."""
+
+import typing
+from pathlib import Path
+
+import core_fixtures
+import jubilant
+import pytest
+import pytest_asyncio
+import yaml
+
+
+@pytest.fixture(scope="module", name="bind_directory")
+def bind_directory_fixture():
+    """Provide charm metadata."""
+    return Path("bind-operator")
+
+
+@pytest.fixture(scope="module", name="bind_metadata")
+def bind_metadata_fixture(bind_directory):
+    """Provide charm metadata."""
+    yield yaml.safe_load(
+        Path(bind_directory / "charmcraft.yaml").read_text(encoding="UTF-8")
+    )
+
+
+@pytest.fixture(scope="module", name="bind_name")
+def fixture_bind_name(bind_metadata):
+    """Provide charm name from the metadata."""
+    yield bind_metadata["name"]
+
+
+@pytest.fixture(scope="module", name="bind_charm_file")
+def bind_charm_file_fixture(
+    bind_metadata: dict[str, typing.Any],
+    bind_directory: Path,
+    pytestconfig: pytest.Config,
+):
+    """Create bind charm file.
+
+    Args:
+        bind_metadata: bind metadata
+        bind_directory: path to bind's directory
+        pytestconfig: pytest config options
+
+    Returns:
+        charm file's path
+    """
+    charm_file = pytestconfig.getoption("--bind-charm-file")
+    if charm_file:
+        yield f"./{charm_file}"
+        return
+    yield core_fixtures.create_charm_file(bind_metadata, bind_directory)
+
+
+@pytest_asyncio.fixture(scope="module", name="bind")
+async def bind_fixture(
+    juju: jubilant.Juju,
+    bind_charm_file: str,
+    bind_name: str,
+    pytestconfig: pytest.Config,
+):
+    """Deploy the charm."""
+    use_existing = pytestconfig.getoption("--use-existing", default=False)
+    if use_existing:
+        return
+
+    juju.deploy(bind_charm_file, bind_name, resources={})
+    juju.wait(lambda status: jubilant.all_active(status, bind_name))
